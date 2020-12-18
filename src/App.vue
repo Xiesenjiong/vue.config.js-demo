cc8d5b48be1c9ff2c1bf5a5ca414996713c40c32
xjiojio <375789796@qq.com>
2020-12-19 00:55:42 +0800

    vue.config.js配置

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input v-model="message">
+    <input :value="message" @input="handleChange">
+    {{message}}
+    <!-- <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />-->
+    <todo-list>
+      <todo-item
+        @delete="handleDelete"
+        v-for="item in list"
+        :key="item.title"
+        data-test="dd"
+        :title="item.title"
+        :del="item.del"
+      >
+        <template v-slot:pre-icon="{value}">
+          <span>⏩ {{value}}</span>
+        </template>
+      </todo-item>
+    </todo-list>
+    <el-date-picker></el-date-picker>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from "./components/TodoItem.vue";
+import TodoList from "./components/TodoList.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    TodoItem,
+    TodoList
+  },
+  data() {
+    return {
+      message: "hello world",
+      list: [
+        {
+          title: "课程1",
+          del: false
+        },
+        {
+          title: "课程2",
+          del: true
+        },
+        {
+          title: "课程3",
+          del: false
+        },
+        {
+          title: "课程4",
+          del: true
+        }
+      ]
+    };
+  },
+  methods: {
+    handleDelete(val) {
+      console.log("handleDelete", val);
+    },
+    handleChange(e) {
+      this.message = e.target.value
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ vue.config.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const webpack = require("webpack");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+  publicPath: "./",
+  outputDir: "dist",
+  assetsDir: "static",
+  configureWebpack: {
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      }),
+
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css",
+      }),
+
+      new OptimizeCSSAssetsPlugin({}),
+    ],
+    resolve: {
+      alias: {},
+    },
+  },
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule("svg");
+
+    // 清除已有的所有 loader
+    // 如果你不这样做，接下来的 loader 会附加在该规则现有的 loader 之后
+    svgRule.uses.clear();
+
+    // 添加要替换的 loader
+    svgRule.use("vue-svg-loader").loader("vue-svg-loader");
+  },
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        bypass: function(req, res) {
+          if (req.headers.accept.indexOf("html") !== -1) {
+            console.log("Skipping proxy for browser request.");
+            return "/index.html";
+          } else if (process.env.MOCK !== "none") {
+            const name = req.path
+              .split("/api/")[1]
+              .split("/")
+              .join("_");
+            const mock = require(`./mock/${name}`);
+            const result = mock(req.method);
+            delete require.cache[require.resolve(`./mock/${name}`)];
+            return res.send(result);
+          }
+        },
+      },
+    },
+  },
+  css: {
+    loaderOptions: {},
+  },
+};
